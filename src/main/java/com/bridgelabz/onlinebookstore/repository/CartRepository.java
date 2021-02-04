02edea0a030f4f15fbf3e48afa036edcd0773a26
ManaliParab12 <manaliparab10@gmail.com>
2021-02-04 19:27:33 +0530

    Online Bookstore Backend

---
+++ src/main/java/com/bridgelabz/onlinebookstore/repository/CartRepository.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.onlinebookstore.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bridgelabz.onlinebookstore.model.Book;
+import com.bridgelabz.onlinebookstore.model.Cart;
+import com.bridgelabz.onlinebookstore.model.User;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+	Optional<Cart> findByBookAndUser(Book book, User user);
+	
+	List<Cart> findAllBooksByUser(User user);
+
+	Cart deleteAllBooksByUser(User user);
+
+	void deleteCartByUserEquals(User user);
+
+}
---
+++ src/main/java/com/bridgelabz/onlinebookstore/service/CartService.java
@@ -0,0 +1,82 @@
+package com.bridgelabz.onlinebookstore.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.onlinebookstore.dto.CartDTO;
+import com.bridgelabz.onlinebookstore.dto.ResponseDTO;
+import com.bridgelabz.onlinebookstore.exception.UserException;
+import com.bridgelabz.onlinebookstore.model.Cart;
+import com.bridgelabz.onlinebookstore.repository.BookRepository;
+import com.bridgelabz.onlinebookstore.repository.CartRepository;
+import com.bridgelabz.onlinebookstore.repository.UserRepository;
+import com.bridgelabz.onlinebookstore.utility.Token;
+
+
+
+@Service
+public class CartService implements ICartService {
+	
+	@Autowired
+	private CartRepository cartRepository;	
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	
+	
+	@Override
+	public ResponseDTO addBookToCart(CartDTO cartDTO) {
+		Cart cart = new Cart(cartDTO);
+		cartRepository.save(cart);
+		return new ResponseDTO("Book Added to cart");
+	}
+
+
+	@Override
+	public ResponseDTO updateCart(String token, int bookId, int quantity) {
+		int userId = Token.decodeToken(token);
+		Optional<Cart> carts = cartRepository.findByBookAndUser(bookRepository.findById(bookId).get()
+	                , userRepository.findById(userId).get());
+	    Cart cart = carts.orElseThrow(() -> new UserException("INVALID_USER"));
+	    cart.setQuantity(quantity);
+	    cartRepository.save(cart);
+	    return new  ResponseDTO("Book Quantity updated"); 
+	}
+	
+	@Override
+	public List<Cart> getListOfBooksInCart(String email) {
+	    return cartRepository.findAllBooksByUser(userRepository.findByEmail(email).get());
+	}
+	 
+	@Override
+	public ResponseDTO removeBookFromCart(int bookId, String token) {
+		int userId = Token.decodeToken(token);
+		Optional<Cart> carts = cartRepository.findByBookAndUser(bookRepository.findById(bookId).get()
+		                , userRepository.findById(userId).get());
+		Cart cart = carts.orElseThrow(() -> new RuntimeException("INVALID_BOOK_ID"));
+		cartRepository.deleteById(cart.getId());
+		return new ResponseDTO("Book Removed from cart"); 	
+	}
+
+
+//	@Override
+//	public Cart removeAllBooks(String token) {
+//		int userId = Token.decodeToken(token);
+//		return cartRepository.deleteAllBooksByUser(userRepository.findById(userId).get());		
+//	}
+	
+	
+	@Override
+	   public ResponseDTO  removeAllBooksFromCart(String email) {
+	       cartRepository.deleteCartByUserEquals(userRepository.findByEmail(email).get());
+	       return new ResponseDTO("Cart is empty"); 
+	    }
+}
